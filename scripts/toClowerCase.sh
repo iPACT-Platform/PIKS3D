086659f1014f7f51547381bc049cf4187e860435
Lianhua Zhu <zhulianhua121@gmail.com>
2018-11-07 19:45:04 +0000

    clean code lower/upper case

--- scripts/toClowerCase.sh
+++ scripts/toClowerCase.sh
@@ -8,4 +8,17 @@ for i in $(cat lowerCase.txt); do
     goodi=$(echo ${i}|tr '[:upper:]' '[:lower:]')
     #echo ${i} ${goodi}
     sed -i -e "s/${i}/${goodi}/g" ../src/*.F90
+    sed -i -e "s/SHifT/shift/g" ../src/*.F90
+    sed -i -e "s/doUBLE/DOUBLE/g" ../src/*.F90
+    sed -i -e "s/COor/COOR/g" ../src/*.F90
+    sed -i -e "s/WorLD/WORLD/g" ../src/*.F90
+    sed -i -e "s/PARAfile/PARAFILE/g" ../src/*.F90
+    sed -i -e "s/IMAGEfile/IMAGEFILE/g" ../src/*.F90
+    sed -i -e "s/ERRor/ERROR/g" ../src/*.F90
+    sed -i -e "s/MPI_status_SIZE/MPI_STATUS_SIZE/g" ../src/*.F90
+    sed -i -e "s/ISend/ISEND/g" ../src/*.F90
+    sed -i -e "s/OMP do/OMP DO/g" ../src/*.F90
+    sed -i -e "s/OMP end/OMP END/g" ../src/*.F90
+    sed -i -e "s/OMP enddo/OMP ENDDO/g" ../src/*.F90
+    sed -i -e "s/OMP end do/OMP END DO/g" ../src/*.F90
 done
