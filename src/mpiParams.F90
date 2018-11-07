91f7b4699d736c03651a02c2d938049986e8c5ff
Lianhua Zhu <zhulianhua121@gmail.com>
2018-11-07 20:25:54 +0000

    Revert "LOGICAL"
    This reverts commit 841d6f60a56a093b78bd4a8933f46374fee79ce2.

--- src/mpiParams.F90
+++ src/mpiParams.F90
@@ -79,8 +79,8 @@ contains
         intege :: complete, direction, partial, shift
         intege :: MPI_ERR
         intege, dimension(1:mpi_dim) :: dims, mpi_coords
-        logical, dimension(1:mpi_dim) :: peiodic
-        logical :: eorder
+        LOGICAL, dimension(1:mpi_dim) :: peiodic
+        LOGICAL :: eorder
         intege :: j, k
         intege, dimension(6) :: my_ext
         
